0a8df2bcbdf995ccb57721b9b765597e5193e173
Jesse <jessehwang86@yahoo.com>
2017-10-11 21:54:59 -0400

    Initial Commit

---
+++ Segue/SecondViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SecondViewController.swift
+//  Segue
+//
+//  Created by Jesse on 10/11/17.
+//  Copyright © 2017 Jesse Hwang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var textPassedOver: String?
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        label.text = textPassedOver
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Segue/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Segue
+//
+//  Created by Jesse on 10/11/17.
+//  Copyright © 2017 Jesse Hwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "goToSecondScreen", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToSecondScreen" {
+            let destinationVC = segue.destination as! SecondViewController
+            
+            destinationVC.textPassedOver = textField.text!
+        }
+    }
+}
+
